503b5ecd89fc7da0776da502aa636d1ce99f7a59
samuel muzy <samuelmuzyfernandes4@gmail.com>
2024-12-03 02:39:00 -0300

    mudança no button bebidas

--- src/components/CrieUmaConta.js
+++ src/components/CrieUmaConta.js
@@ -1,15 +1,13 @@
 import { ButtonConta, DivSemConta, ImagemConta } from "../styles/CardDeUsuarios"
 import imagemConta from '../assets/botao-redondo-da-conta-com-o-usuario-dentro.png'
-import { Navigate } from "react-router-dom"
+import { useNavegar } from "../hooks/useNavegar"
 export const CrieUmaConta = () =>{
-    const navegarLogin = () =>{
-        Navigate("/login")
-    }
+    const {NavegarLogin} = useNavegar()
     return(
         <DivSemConta>
             <ImagemConta src={imagemConta} alt="Fazer Conta"/>
             <h2>Logue em uma conta</h2>
-            <ButtonConta onClick={navegarLogin}>Logar</ButtonConta>
+            <ButtonConta onClick={NavegarLogin}>Logar</ButtonConta>
         </DivSemConta>
     )
 }
--- src/components/buttons/ButtonAdd.js
+++ src/components/buttons/ButtonAdd.js
@@ -5,7 +5,7 @@ const ButtonAdd = (props) => {
   return (
     <StyledWrapper>
       <button onClick={props.click} type="button" className="button">
-        <span className="button__text">Add Item</span>
+        <span className="button__text">Adicionar</span>
         <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width={24} viewBox="0 0 24 24" strokeWidth={2} strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height={24} fill="none" className="svg"><line y2={19} y1={5} x2={12} x1={12} /><line y2={12} y1={12} x2={19} x1={5} /></svg></span>
       </button>
     </StyledWrapper>
